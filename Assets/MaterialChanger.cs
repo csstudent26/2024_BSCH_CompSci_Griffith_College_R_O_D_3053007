911eec6cacbdab38f5e392eb2cb1a01f34284052
Rory O Donoghue <irelander99@mail.com>
2024-04-13 21:04:27 +0100

    Commit 09: Commenting Code in Script 'MaterialChanger

--- Assets/MaterialChanger.cs
+++ Assets/MaterialChanger.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class MaterialChanger : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Material testMat;
+    public Material testMat;//Creating a Public Variable, which is accessable to all
     void Start()
     {
-        testMat = GetComponent<MeshRenderer>().material;
+        testMat = GetComponent<MeshRenderer>().material;// Getting Value of Objects MeshRenderer and assiging it to 'testMat
     }
 
     // Update is called once per frame
@@ -19,6 +19,6 @@ public class MaterialChanger : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision){
 
-        testMat.color = Random.ColorHSV();
+        testMat.color = Random.ColorHSV();//Using Built In Random Class to Chose Random Color
     }
 }

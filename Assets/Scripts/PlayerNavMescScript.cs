fb5caef1fdeb0d35878eb9ad25c44ed8ca16f521
Rory O Donoghue <irelander99@mail.com>
2024-05-05 15:52:46 +0100

    Commit 52 : Adding EXERCISE 7, and Enemy and Player NavMesh scripts

---
+++ Assets/Scripts/PlayerNavMescScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavMescScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public NavMeshAgent agent;
+
+    public float currentVelocity;
+    
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+          //get the velocity of the player
+        currentVelocity = agent.velocity.magnitude;
+        //set the speed of the animator to the velocity of the player
+        animator.SetFloat("velocity", currentVelocity);
+        
+        //move towards the mouseclick using navmesh
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        
+    }
+}
